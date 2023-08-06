bd5795490b26e2fc409b797ddb5a25dcabd6261f
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-08-05 21:17:13 -0300

    configurando o projeto 2 do iginite

--- reatc_js/ignite_rockeseat/02-ignite-timer/src/App.tsx
+++ reatc_js/ignite_rockeseat/02-ignite-timer/src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <div>Ignite Timer</div>
+    <ThemeProvider theme={defaultTheme}>
+      <div>Ignite Timer</div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }

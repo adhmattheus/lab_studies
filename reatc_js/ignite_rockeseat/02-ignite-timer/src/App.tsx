27e50a2a09893d9e3eb685261d28a6b0fd028b67
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-08-05 21:25:26 -0300

    add react-router-dom

--- reatc_js/ignite_rockeseat/02-ignite-timer/src/App.tsx
+++ reatc_js/ignite_rockeseat/02-ignite-timer/src/App.tsx
@@ -1,12 +1,16 @@
 import { GlobalStyle } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Ignite Timer</div>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }

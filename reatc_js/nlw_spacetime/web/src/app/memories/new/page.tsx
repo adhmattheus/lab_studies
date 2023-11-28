cbac4c0ebeb1242c6b3b823f7ae1c6354df68657
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-11-28 10:53:07 -0300

    preview da imagem da nova memória

--- reatc_js/nlw_spacetime/web/src/app/memories/new/page.tsx
+++ reatc_js/nlw_spacetime/web/src/app/memories/new/page.tsx
@@ -1,7 +1,8 @@
+import { MediaPicker } from "@/components/MediaPicker";
 import { Camera, ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
-export default function NewMemorie() {
+export default function NewMemoriy() {
   return (
     <div className="flex flex-1 flex-col gap-4">
       <Link href="/" className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100">
@@ -19,14 +20,13 @@ export default function NewMemorie() {
             Anexar mídia
           </label>
 
-
           <label htmlFor="isPublic" className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100">
             <input type="checkbox" className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500" name="isPublic" id="isPublic" value="true" />
             Tornar memória pública
           </label>
         </div>
 
-        <input type="file" id="media" className="invisible h-0 w-0" />
+        <MediaPicker />
 
         <textarea
           name="content"
